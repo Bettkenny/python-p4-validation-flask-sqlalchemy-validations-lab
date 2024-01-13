436472de90bb2beeb981518adabd68aa029e4d2e
YOUR NAME <YOUR@EMAIL.com>
2024-01-13 10:33:53 +0300

    complete lab

--- server/models.py
+++ server/models.py
@@ -1,17 +1,31 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+from sqlalchemy.exc import IntegrityError
+
 db = SQLAlchemy()
 
 class Author(db.Model):
     __tablename__ = 'authors'
     
     id = db.Column(db.Integer, primary_key=True)
-    name= db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String, unique=True, nullable=False)
     phone_number = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators 
+    @validates('name')
+    def validate_name(self, key, name):
+        
+        if not name:
+            raise ValueError("Author name cannot be empty.")
+        return name
+
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        
+        if phone_number and not phone_number.isdigit() or len(phone_number) != 10:
+            raise ValueError("Invalid phone number. It should have exactly 10 digits.")
+        return phone_number
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -27,8 +41,26 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    @validates('title')
+    def validate_title(self, key, title):
+       
+        if not title:
+            raise ValueError("Post title cannot be empty.")
+        return title
 
+    @validates('content')
+    def validate_content(self, key, content):
+       
+        if len(content) < 250:
+            raise ValueError("Post content must be at least 250 characters long.")
+        return content
+
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        
+        if len(summary) > 250:
+            raise ValueError("Post summary cannot exceed 250 characters.")
+        return summary
 
     def __repr__(self):
-        return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+        return f'Post(id={self.id}, title={self.title}, content={self.content}, summary={self.summary})'
